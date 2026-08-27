f636a3846f4686a8bc0e84b4927fb82c4b297651
Ankit Kumar <ankitsuman8298@gmail.com>
2026-08-27 11:58:58 +0530

    first

--- ArrayLists.java
+++ ArrayLists.java
@@ -149,7 +149,7 @@ public class ArrayLists{
 }
 */
 
-
+/* 
 //Pair Sum - 1
 //Brute Force
 public static boolean pairSum(ArrayList<Integer> list, int target){
@@ -186,5 +186,36 @@ public static void main(String args[]){
     System.out.println(pairSum(list, 5));
     System.out.println(pairSum1(list, 5));
 }
+*/
 
-
+//Pair Sum - 2
+//List = [11,15,6,8,9,10]
+public static boolean pairSum2(ArrayList<Integer> list, int target){
+    int bp = -1;
+    int n = list.size();
+    for(int i=0; i<list.size(); i++){
+        if(list.get(i) > list.get(i+1)){  //Breaking point
+            bp = i;
+            break;
+        }
+    }
+    int lp = bp + 1; //smallest
+    int rp = bp;    //largest
+    while(lp != rp){
+        if(list.get(lp) + list.get(rp) == target){
+            return true;
+        }
+        if(list.get(lp) + list.get(rp) < target){
+            lp = (lp+1)%n;
+        }else{
+            rp = (n-rp-1)%n;
+        }
+    }
+    return false;
+}
+public static void main(String args[]){
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(11);list.add(15);list.add(6);list.add(8);
+    list.add(9);list.add(10);
+    System.out.println(pairSum2(list, 16));
+}
